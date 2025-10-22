700a9684af442b2193741fb0051b3f8e46017d91
Kartofechka <126353546+Kartofechka@users.noreply.github.com>
2025-10-22 13:27:59 +0300

    Add files via upload

---
+++ user_session/main.py
@@ -0,0 +1,65 @@
+import uuid
+import pandas as pd
+from datetime import datetime, timedelta
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+templates = Jinja2Templates(directory="templates")
+USERS = "users.csv"
+SESSION_TTL = timedelta(10)
+sessions = {}
+white_urls = ["/", "/login", "/logout"]
+
+#Контролоь авторизации и сессии
+@app.middleware("http")
+async def check_session(request: Request, call_next):
+    path = request.url.path
+    if path.startswith("/static") or path.startswith("/assets") or path in white_urls:
+        return await call_next(request)
+
+    session_id = request.cookies.get("session_id")
+    if not session_id or session_id not in sessions:
+        return RedirectResponse(url="/login")
+
+    created_session = sessions[session_id]
+    if datetime.now() - created_session > SESSION_TTL:
+        del sessions[session_id]
+        return RedirectResponse(url="/login")
+
+    return await call_next(request)
+
+
+
+@app.get("/", response_class=HTMLResponse)
+@app.get("/login", response_class=HTMLResponse)
+def get_login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post("/login")
+def login(request: Request,
+            username: str = Form(...),
+            password: str = Form(...)):
+    users = pd.read_csv(USERS)
+    if username in users['user']: 
+        if str(users[users['user'] == username]).values[0][1] == password:
+            session_id = str(uuid.uuid4())
+            sessions[session_id] = datetime.now()
+            response = RedirectResponse(url=f"/home/{username}", status_code=302)
+            response.set_cookie(key="session_id", value=session_id)
+            return response
+    return templates.TemplateResponse("login.html",
+                                       {"request": request,
+                                        "error": "Глупец, введи правильный логин и пароль"})
+
+
+@app.get("/home/admin", response_class=HTMLResponse)
+def login_page(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request})
+
