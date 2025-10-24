5a923c04980b523dc74a33dbe58e806fc46be36d
Kartofechka <126353546+Kartofechka@users.noreply.github.com>
2025-10-24 11:54:07 +0300

    Update main.py

--- user_session/main.py
+++ user_session/main.py
@@ -47,17 +47,28 @@ def login(request: Request,
             username: str = Form(...),
             password: str = Form(...)):
     users = pd.read_csv(USERS)
-    if username in users['user']: 
-        if str(users[users['user'] == username]).values[0][1] == password:
+    if username in users['user'].values[0]: 
+        if str(users[users['user'] == username].values[0][1]) == password:
             session_id = str(uuid.uuid4())
             sessions[session_id] = datetime.now()
             response = RedirectResponse(url=f"/home/{username}", status_code=302)
             response.set_cookie(key="session_id", value=session_id)
             return response
+        return templates.TemplateResponse("login.html",
+                                       {"request": request,
+                                        "error": "Глупец, введи правильный пароль"})
     return templates.TemplateResponse("login.html",
                                        {"request": request,
-                                        "error": "Глупец, введи правильный логин и пароль"})
+                                        "error": "Глупец, введи правильный логин"})
 
+
+@app.get("/logout", response_class=HTMLResponse)
+def logout(request: Request):
+    session_id = request.cookies.get("session_id")
+    del sessions[session_id]
+    return templates.TemplateResponse("login.html", {"request": request,
+                                        "message": "Вы вышли из системы",
+                                        "url": "/login"})
 
 @app.get("/home/admin", response_class=HTMLResponse)
 def login_page(request: Request):
